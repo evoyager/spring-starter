534d73aa610f5151551488f9af2e91d6c25955db
Ievgen Gusar <ievgen_gusar@epam.com>
2025-01-07 19:40:30 +0200

    26.Java-based-Configuration

---
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -0,0 +1,37 @@
+package com.dmdev.spring.config;
+
+import com.dmdev.spring.database.pool.ConnectionPool;
+import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.spring.database.repository.UserRepository;
+import com.dmdev.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.stereotype.Component;
+
+//@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.dmdev.spring",
+        useDefaultFilters = false,
+        includeFilters = {
+                @Filter(type = FilterType.ANNOTATION, value = Component.class),
+                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+                @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
+        })
+public class ApplicationConfiguration {
+
+        @Bean
+        @Scope(BeanDefinition.SCOPE_SINGLETON)
+        public ConnectionPool pool2(@Value("${db.username}") String username) {
+                return new ConnectionPool(username, 20);
+        }
+
+        @Bean
+        public UserRepository userRepository2(ConnectionPool pool2) {
+                return new UserRepository(pool2);
+        }
+
+}
--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -7,15 +7,19 @@ import com.dmdev.spring.database.entity.Company;
 import com.dmdev.spring.database.pool.ConnectionPool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
+
 @Transaction
 @Auditing
 @Repository
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
     private final ConnectionPool pool1;
