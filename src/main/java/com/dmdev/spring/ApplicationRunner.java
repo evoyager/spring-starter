98f3304e73b95178ae361493a2df070bc1b01265
Ievgen Gusar <ievgen_gusar@epam.com>
2025-01-09 21:03:41 +0200

    36.-Properties

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApplicationRunner {
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationRunner.class, args);
+        var context = SpringApplication.run(ApplicationRunner.class, args);
+        System.out.println(context.getBeanDefinitionCount());
     }
 }
